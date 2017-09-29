f1635c5d0631a84d00548024e0efb7526624aede
Nag <nagabhushanamn@gmail.com>
2017-09-29 19:04:55 +0530

    final commit

--- NG/NG-Todo-app/app.js
+++ NG/NG-Todo-app/app.js
@@ -1,19 +1,35 @@
 
-
-
 (function () {
 
     let mod = angular.module('todo-app', ['ngResource', 'ui.router']);
 
-    mod.config(function ($stateProvider, $urlRouterProvider) {
+
+    mod.config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
+
+        //$httpProvider.interceptors.push('myInterceptor');
+
         $urlRouterProvider.otherwise('/');
 
         let homeState = {
             name: 'home',
             url: '/',
-            templateUrl: 'templates/todo-home.html'
+            templateUrl: 'templates/home.html'
         };
-        let allState = {
+        let loginState = {
+            name: 'login',
+            url: '/login',
+            templateUrl: 'templates/login.html',
+            controller: function ($scope, $state, authService) {
+                $scope.loginForm = {};
+                $scope.doLogin = function () {
+                    authService.login($scope.loginForm)
+                        .then(() => {
+                            $state.go('todos');
+                        });
+                }
+            }
+        };
+        let todosState = {
             name: 'todos',
             url: '/todos',
             resolve: {
@@ -27,13 +43,14 @@
 
         $stateProvider
             .state(homeState)
-            .state(allState)
+            .state(todosState)
+            .state(loginState)
 
     });
 
     //-------------------------------------------------
 
-    mod.controller('TodoAppController', function ($scope, todos, Todo) {
+    mod.controller('TodoAppController', function ($scope, $http, todos, Todo, authService) {
         $scope.todos = todos;
 
         $scope.addNewTodo = function (event) {
@@ -41,10 +58,17 @@
             if (!val || event.which !== 13) return;
             let newTodo = new Todo();
             angular.extend(newTodo, { title: val, completed: false });
-            newTodo.$save(function (todo) {
-                todos.push(todo);
-                event.target.value = '';
-            });
+            // newTodo.$save(function (todo) {
+            //     todos.push(todo);
+            //     event.target.value = '';
+            // });
+            console.log(authService.getToken());
+            let apiUrl = "http://localhost:3000/api/todos?access_token=" + authService.getToken();
+            $http.post(apiUrl, newTodo)
+                .then((resp) => {
+                    todos.push(resp.data);
+                    event.target.value = '';
+                });
         }
         $scope.deleteTodo = function (id) {
             Todo.delete({ id }, function (resp) {
@@ -53,6 +77,10 @@
             });
         }
 
+        $scope.toggleCompleteTodo = function (todo) {
+            Todo.update({ id: todo.id }, todo, function (todo) {
+            });
+        }
 
     })
 
@@ -62,9 +90,52 @@
 
     mod.factory('Todo', function ($resource) {
         let api = "http://localhost:3000/api/todos/:id";
-        let Todo = $resource(api, { id: '@id' });
+        let Todo = $resource(api, { id: '@id' }, { 'update': { method: 'PUT' } });
         return Todo;
     });
+
+
+    mod.factory('authService', function ($http, $q) {
+        let loginUrl = "http://0.0.0.0:3000/api/Users/login";
+
+        return serv = {
+            token: '',
+            setToken: function (token) {
+                this.token = token
+            },
+            getToken: function () {
+                return this.token;
+            },
+            isAuthenticated() {
+                return this.token !== "";
+            },
+            login: function (loginForm) {
+                let defer = $q.defer();
+                $http.post(loginUrl, loginForm)
+                    .then((resp) => {
+                        console.log(resp);
+                        this.setToken(resp.data.id);
+                        defer.resolve();
+                    }, (error) => {
+                        console.log('login fail');
+                    });
+                return defer.promise;
+            }
+        }
+
+    });
+
+    mod.factory('myInterceptor', ['$log', function ($log) {
+        var myInterceptor = {
+            request: function (config) {
+                console.log('request interceptor...');
+            }
+        };
+        return myInterceptor;
+    }]);
+
+
+
 
 
 })();
